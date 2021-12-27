d25bd2e300f899770023c26016e90697539d08ce
nostalgebraist <nostalgebraist@gmail.com>
2021-12-27 12:19:16 -0800

    work

--- improved_diffusion/nn.py
+++ improved_diffusion/nn.py
@@ -195,8 +195,10 @@ class CheckpointFunction(th.autograd.Function):
             shallow_copies = [x.view_as(x) for x in ctx.input_tensors]
             output_tensors = ctx.run_function(*shallow_copies)
         include_mask = [og.requires_grad for og in output_grads]
-        for t, og in zip(output_tensors, output_grads):
-            print((t.requires_grad, og is None))
+        for t in output_tensors:
+            print(t.requires_grad)
+        for og in output_grads:
+            print(og is None)
         input_grads = th.autograd.grad(
             [t for t, og in zip(output_tensors, output_grads) if og is not None],
             ctx.input_tensors + ctx.input_params,

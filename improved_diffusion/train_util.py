3bd5d476a776ce271923b4147e8807f890e09c2a
nostalgebraist <nostalgebraist@gmail.com>
2022-02-01 15:14:33 -0800

    work

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -8,7 +8,7 @@ from collections import defaultdict
 import blobfile as bf
 import numpy as np
 import torch as th
-import torch.distributed as dist
+import torch.distributed as dist_
 from torch.nn.parallel.distributed import DistributedDataParallel as DDP
 from torch.optim import AdamW
 
@@ -29,6 +29,23 @@ from .image_datasets import tokenize
 # We found that the lg_loss_scale quickly climbed to
 # 20-21 within the first ~1K steps of training.
 INITIAL_LOG_LOSS_SCALE = 20.0
+
+class FakeDist:
+    def get_world_size(self):
+        return 1
+
+    def get_rank(self):
+        return 0
+
+    def barrier(self):
+        pass
+
+
+def get_dist(use_deepspeed):
+    if use_deepspeed:
+        return FakeDist()
+    else:
+        return dist_
 
 
 class WrapperForDeepspeed(th.nn.Module):
@@ -118,7 +135,7 @@ class TrainLoop:
 
         self.step = 0
         self.resume_step = 0
-        self.global_batch = self.batch_size * dist.get_world_size()
+        self.global_batch = self.batch_size * get_dist(use_deepspeed=self.use_deepspeed).get_world_size()
 
         # text_params, self.text_param_names = [], []
         text_params, text_param_names = defaultdict(list), defaultdict(list)
@@ -269,7 +286,7 @@ class TrainLoop:
                 find_unused_parameters=False,
             )
         else:
-            if dist.get_world_size() > 1:
+            if get_dist(use_deepspeed=self.use_deepspeed).get_world_size() > 1:
                 logger.warn(
                     "Distributed training requires CUDA. "
                     "Gradients will not be synchronized properly!"
@@ -282,7 +299,7 @@ class TrainLoop:
 
         if resume_checkpoint:
             self.resume_step = parse_resume_step_from_filename(resume_checkpoint)
-            if dist.get_rank() == 0:
+            if get_dist(use_deepspeed=self.use_deepspeed).get_rank() == 0:
                 logger.log(f"loading model from checkpoint: {resume_checkpoint}...")
                 sd = dist_util.load_state_dict(
                     resume_checkpoint, map_location=dist_util.dev()
@@ -335,7 +352,7 @@ class TrainLoop:
         main_checkpoint = find_resume_checkpoint() or self.resume_checkpoint
         ema_checkpoint = find_ema_checkpoint(main_checkpoint, self.resume_step, rate)
         if ema_checkpoint:
-            if dist.get_rank() == 0:
+            if get_dist(use_deepspeed=self.use_deepspeed).get_rank() == 0:
                 logger.log(f"loading EMA from checkpoint: {ema_checkpoint}...")
                 state_dict = dist_util.load_state_dict(
                     ema_checkpoint, map_location=dist_util.dev()
@@ -697,7 +714,7 @@ class TrainLoop:
     def save(self):
         def save_checkpoint(rate, params):
             state_dict = self._master_params_to_state_dict(params)
-            if dist.get_rank() == 0:
+            if get_dist(use_deepspeed=self.use_deepspeed).get_rank() == 0:
                 logger.log(f"saving model {rate}...")
                 if not rate:
                     filename = f"model{(self.step+self.resume_step):06d}.pt"
@@ -712,7 +729,7 @@ class TrainLoop:
 
         logger.log("saving opt...")
 
-        if dist.get_rank() == 0:
+        if get_dist(use_deepspeed=self.use_deepspeed).get_rank() == 0:
             with bf.BlobFile(
                 bf.join(get_blob_logdir(), f"opt{(self.step+self.resume_step):06d}.pt"),
                 "wb",
@@ -724,7 +741,7 @@ class TrainLoop:
         if self.autosave:
             save_progress_to_gcs(step=self.step+self.resume_step, ema_rates=self.ema_rate, autosave_dir=self.autosave_dir)
 
-        dist.barrier()
+        get_dist(use_deepspeed=self.use_deepspeed).barrier()
 
     def _master_params_to_state_dict(self, master_params):
         if self.use_fp16:

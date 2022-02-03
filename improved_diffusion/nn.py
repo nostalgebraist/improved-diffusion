4dfb83957dcb6d518cf4813a82a7cdd58e0727ae
nostalgebraist <nostalgebraist@gmail.com>
2022-02-02 16:05:55 -0800

    work

--- improved_diffusion/nn.py
+++ improved_diffusion/nn.py
@@ -237,11 +237,11 @@ def checkpoint(func, inputs, params, flag, final_nograd=0):
     """
     if flag:
         import deepspeed
-        args = tuple(inputs) + tuple(params)
         if deepspeed.checkpointing.is_configured():
             # print('using deepspeed checkpointing')
-            return deepspeed.checkpointing.checkpoint(func, *args)
+            return deepspeed.checkpointing.checkpoint(func, *inputs)
         # print(f"ckpt final_nograd: {final_nograd}")
+        args = tuple(inputs) + tuple(params)
         return CheckpointFunction.apply(func, len(inputs), final_nograd, *args)
     else:
         return func(*inputs)

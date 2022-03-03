8af78b51a20401dc34f6831ac51ece8874900945
nostalgebraist <nostalgebraist@gmail.com>
2022-03-03 13:29:27 -0800

    work

--- improved_diffusion/respace.py
+++ improved_diffusion/respace.py
@@ -127,4 +127,5 @@ class _WrappedModel:
         new_ts = map_tensor[ts]
         if self.rescale_timesteps:
             new_ts = new_ts.float() * (1000.0 / self.original_num_steps)
+        print(repr(new_ts))
         return self.model(x, new_ts, **kwargs)

e3d0e519d9de1ccf46a21637a5cfa4e24cc22144
nostalgebraist <nostalgebraist@gmail.com>
2022-03-03 13:32:56 -0800

    work

--- improved_diffusion/respace.py
+++ improved_diffusion/respace.py
@@ -123,6 +123,7 @@ class _WrappedModel:
         self.original_num_steps = original_num_steps
 
     def __call__(self, x, ts, **kwargs):
+        print(repr(ts))
         map_tensor = th.tensor(self.timestep_map, device=ts.device, dtype=ts.dtype)
         new_ts = map_tensor[ts]
         if self.rescale_timesteps:

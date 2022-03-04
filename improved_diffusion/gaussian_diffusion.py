e6a812050827564664ccae6200e0ef2cc07dc227
nostalgebraist <nostalgebraist@gmail.com>
2022-03-03 16:29:48 -0800

    work

--- improved_diffusion/gaussian_diffusion.py
+++ improved_diffusion/gaussian_diffusion.py
@@ -697,7 +697,6 @@ class GaussianDiffusion:
                 xstart = xstart.clamp(-1, 1)
 
             alpha_bar_t2 = _extract_into_tensor(self.alphas_cumprod, t2_, x.shape)
-            print((t2_, alpha_bar_t2))
             return (
                 xstart * th.sqrt(alpha_bar_t2) + th.sqrt(1 - alpha_bar_t2) * eps
             ), xstart
@@ -718,9 +717,6 @@ class GaussianDiffusion:
         eps4 = model_step(x3, t2)
 
         eps_prime = (eps1 + 2 * eps2 + 2 * eps3 + eps4) / 6
-        print((t, t_mid, t2))
-
-        print(eps_prime)
         # eps_prime = eps1
         x_new, pred = transfer(x, eps_prime, t1, t2)
         # eps_prime = eps1  # debug

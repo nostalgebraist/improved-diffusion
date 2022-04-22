85f5077c1277e2aa8c12f09496c44b670fc874be
nostalgebraist <nostalgebraist@gmail.com>
2022-04-21 21:18:05 -0700

    work

--- improved_diffusion/image_datasets.py
+++ improved_diffusion/image_datasets.py
@@ -369,8 +369,6 @@ def load_superres_data(data_dir, batch_size, large_size, small_size, class_cond=
         # regular loader, the low_res key is filled within ImageDataset
         return _dataloader_gen(data, batch_size=batch_size, deterministic=deterministic, pin_memory=pin_memory,
                                prefetch_factor=prefetch_factor,
-                               clip_probs_by_idxs=clip_probs_by_idxs,
-                               clip_prob_middle_pkeep=clip_prob_middle_pkeep,
                                num_workers=num_workers)
     else:
         return _sres_dataloader_gen(

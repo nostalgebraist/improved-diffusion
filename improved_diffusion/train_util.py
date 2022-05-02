be228a234101f74bcd252f2923944f9b9b78c968
nostalgebraist <nostalgebraist@gmail.com>
2022-05-02 12:30:05 -0700

    work

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -472,14 +472,14 @@ class TrainLoop:
             if self.use_profiler:
                 with th.profiler.profile(
                     with_stack=True,
-                    record_shapes=True,
+                    # record_shapes=True,
                     profile_memory=True,
                     # with_flops=True
                 ) as _p:
                     self.run_step(batch, cond, verbose = (self.step % self.log_interval == 0))
                 print(
                     _p.key_averages(
-                        group_by_input_shape=True,
+                        # group_by_input_shape=True,
                         group_by_stack_n=5
                     ).table(
                         # sort_by="self_cuda_time_total",

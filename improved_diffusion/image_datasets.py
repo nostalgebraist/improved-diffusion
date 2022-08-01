8146a922e67bbc3845f22fb7e51d30aed99458b0
nostalgebraist <nostalgebraist@gmail.com>
2022-08-01 11:20:01 -0700

    fix

--- improved_diffusion/image_datasets.py
+++ improved_diffusion/image_datasets.py
@@ -161,8 +161,8 @@ def load_data(
         n_with_px_scale = len(set(text_file_to_image_file.values()).intersection(image_file_to_px_scales.keys()))
         print(f"of {n_texts} texts, {n_with_px_scale} have px scales (all px scales: {len(image_file_to_px_scales)})")
 
-    n_images_with_capts = len(set(results.keys()).intersection(image_file_to_capt.keys()))
-    print(f"of {len(results)} images, {n_images_with_capts} have capts (all capts: {len(image_file_to_capt)})")
+    n_images_with_capts = len(set(all_files.keys()).intersection(image_file_to_capt.keys()))
+    print(f"of {len(all_files)} images, {n_images_with_capts} have capts (all capts: {len(image_file_to_capt)})")
 
     if clip_probs is not None:
         n_images_with_clip_probs = len(set(all_files).intersection(clip_probs.keys()))

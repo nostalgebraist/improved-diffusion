6277066f8a8131ab6c19077a6da59ab85e9e9d3c
nostalgebraist <nostalgebraist@gmail.com>
2022-08-05 06:36:29 -0700

    unbufferize fake_proj_weight

--- improved_diffusion/text_nn.py
+++ improved_diffusion/text_nn.py
@@ -186,7 +186,7 @@ class BetterMultiheadAttention(torch.nn.MultiheadAttention):
         self.bias_k = self.bias_v = None
         self.add_zero_attn = False
 
-        self.register_buffer("fake_proj_weight", torch.eye(self.qkv_dim, device=device, dtype=torch.float16), persistent=False)
+        # self.register_buffer("fake_proj_weight", torch.eye(self.qkv_dim, device=device, dtype=torch.float16), persistent=False)
 
         # self._reset_parameters()
 
@@ -206,6 +206,8 @@ class BetterMultiheadAttention(torch.nn.MultiheadAttention):
         key = self.k(key)
         value = self.v(value)
 
+        fake_proj_weight = torch.eye(self.qkv_dim, dtype=query.dtype, device=query.device)
+
         in_dtype = query.dtype
 
         attn_output, attn_output_weights = torch.nn.functional.multi_head_attention_forward(
@@ -216,8 +218,8 @@ class BetterMultiheadAttention(torch.nn.MultiheadAttention):
             training=self.training,
             key_padding_mask=None, need_weights=need_weights,
             attn_mask=attn_mask, use_separate_proj_weight=True,
-            q_proj_weight=self.fake_proj_weight, k_proj_weight=self.fake_proj_weight,
-            v_proj_weight=self.fake_proj_weight)
+            q_proj_weight=fake_proj_weight, k_proj_weight=fake_proj_weight,
+            v_proj_weight=fake_proj_weight)
 
         attn_output = attn_output.to(in_dtype)
 

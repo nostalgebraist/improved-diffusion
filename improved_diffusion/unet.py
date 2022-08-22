af1222b9447ad63751c5ee3278e890f7ef7c1f24
nostalgebraist <nostalgebraist@gmail.com>
2022-08-22 14:13:11 -0700

    work

--- improved_diffusion/unet.py
+++ improved_diffusion/unet.py
@@ -1569,12 +1569,11 @@ class UNetModel(nn.Module):
             )
 
             extra_inputs = [
-                *tuple(self.input_blocks.parameters()),
-                *tuple(self.middle_block.parameters()),
-                *tuple(self.output_blocks.parameters()),
-                *tuple(self.out.parameters()),
+                tuple(self.input_blocks.parameters()),
+                tuple(self.middle_block.parameters()),
+                tuple(self.output_blocks.parameters()),
+                tuple(self.out.parameters()),
             ]
-            extra_inputs = tuple(extra_inputs)
 
             self._main_forward_cuda_graphed = make_graphed_callables(self._main_forward, graph_callable_args, extra_inputs=extra_inputs)
 

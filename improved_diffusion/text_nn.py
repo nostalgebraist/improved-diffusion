2a647a4b859025b33999ad432b8e13718b86b693
nostalgebraist <nostalgebraist@gmail.com>
2022-08-22 11:08:54 -0700

    work

--- improved_diffusion/text_nn.py
+++ improved_diffusion/text_nn.py
@@ -124,7 +124,7 @@ class TextEncoder(nn.Module):
         )
 
     def _model_forward(self, x, attn_mask):
-        my_attn_mask = torch.tile(attn_mask.unsqueeze(1).unsqueeze(1), (self.n_heads, tokens.shape[1], 1))
+        my_attn_mask = torch.tile(attn_mask.unsqueeze(1).unsqueeze(1), (self.n_heads, x.shape[1], 1))
         return self.model.forward(x, attn_mask=my_attn_mask)
 
     def compute_embeddings_and_mask(self, tokens, timesteps):

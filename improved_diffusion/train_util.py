d58870717802ad4421d1ce96b99b5574721ac8f4
nostalgebraist <nostalgebraist@gmail.com>
2022-09-08 13:11:57 -0700

    fix

--- improved_diffusion/train_util.py
+++ improved_diffusion/train_util.py
@@ -686,13 +686,13 @@ class TrainLoop:
             n = sum([1 for p in self.model.parameters()])
             grad_nans = sum([not th.isfinite(p).all() for p in self.model.parameters()])
             param_nans = sum([not th.isfinite(p).all() for p in self.model.parameters()])
-            print(f"before: {param_nans} of {n} param nans, {grad_nans} of n grad nans")
+            print(f"before: {param_nans} of {n} param nans, {grad_nans} of {n} grad nans")
         self.grad_scaler.step(self.opt)
         if self.use_esgd:
             n = sum([1 for p in self.model.parameters()])
             grad_nans = sum([not th.isfinite(p).all() for p in self.model.parameters()])
             param_nans = sum([not th.isfinite(p).all() for p in self.model.parameters()])
-            print(f"after: {param_nans} of {n} param nans, {grad_nans} of n grad nans")
+            print(f"after: {param_nans} of {n} param nans, {grad_nans} of {n} grad nans")
         self.grad_scaler.update()
         verboses = [False] * (len(self.ema_rate) - 1) + [True]
         for rate, params, arith_from_step, arith_extra_shift, verbose in zip(
